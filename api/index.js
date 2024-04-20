b4b4f0b75e27e5301d2d25d0f2ff594352ad42b2
fauzibahar <fauzibahar74@gmail.com>
2024-04-20 14:53:32 +0700

    update

--- api/index.js
+++ api/index.js
@@ -12,22 +12,23 @@ connectDB();
 
 // Middleware for handling CORS POLICY
 // Option 1: Allow All Origins with Default of cors(*)
-app.use(cors());
+// app.use(cors());
 // Option 2: Allow Custom Origins
-// app.use(
-//   cors({
-//     origin: 'http://localhost:3000',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type'],
-//   })
-// );
+app.use(
+  cors({
+    origin: "https://book-store-client-weld.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 // Middleware for parsing request body
 app.use(express.json());
 app.use(BookRoute);
 
 app.get("/", (req, res) => {
-  res.json("Ini Adalah API Book");
+  console.log(req);
+  return res.status(234).send("Welcome To Books API");
 });
 
 app.get("*", (req, res) => {
